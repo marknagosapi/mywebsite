daa8320cd52864cfc0a6480394277f1d23afe3ba
mark.nago@3ss.tv <nago.mark@student.ms.sapientia.ro>
2024-05-13 11:18:27 +0300

    Testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>This is a website made by Nago Mark :D</code> 
         </p>
         <a
           className="App-link"
